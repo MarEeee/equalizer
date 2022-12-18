2db035bc651b1b21894f5d4a3a5ebefe608ea333
Efim <pro100efim@gmail.com>
2022-12-18 21:31:44 +0300

    feat: add OrbitControls

--- src/samples/NoiseGrid.js
+++ src/samples/NoiseGrid.js
@@ -1,60 +1,80 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef} from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js";
+import { ImprovedNoise } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/math/ImprovedNoise.js';
 
 
 export default function NoiseGrid(){
-    const canvasRef = useRef()
+    const canvasRef = useRef();
 
     useEffect(()=>{
         const w = window.innerWidth;
         const h = window.innerHeight;
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera( 75, w / h, 0.1, 1000 );
-        const renderer = new THREE.WebGLRenderer()
-        camera.position.z = 5
-        renderer.setSize(w, h)
+        const renderer = new THREE.WebGLRenderer();
+        camera.position.z = 5;
+        renderer.setSize(w, h);
         canvasRef.current.appendChild( renderer.domElement );
 
+
+        // Orbit Controls
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 0, 0);
+        controls.update();
         
+        
+
+        const Noise = new ImprovedNoise()
         const coords = [];
         const colors = [];
         let x = 0;
         let y = 0;
         let z = 0;
-        // COLOR
+        // Color
         let r = 0;
         let g = 0;
         let b = 0;
-        const numCol = 20;
-        const numRow = 20;
+        const numCol = 40;
+        const numRow = 40;
         let vertex;
         let col;
-        const gap = 0.25;
+        const gap = 0.125;
         const offset = { x: -2.2, y: -2.5};
+        let ns;
         for(let i = 0; i < numCol; i++) {
             for(let j = 0; j < numRow; j++) {
-                x = offset.x + i * gap
-                y = offset.y + j * gap
-                vertex = new THREE.Vector3(x, y, z)
-                r = Math.random()
-                g = Math.random()
-                b = Math.random()
-                col = new THREE.Color(r, g, b)
-                coords.push(x,y,z)
-                colors.push(r, g, b)
+                x = offset.x + i * gap;
+                y = offset.y + j * gap;                
+                ns = Noise.noise(x, y, 0);
+                z = ns;
+                vertex = new THREE.Vector3(x, y, z);
+                r = Math.random();
+                g = 0;
+                b = 0;
+                col = new THREE.Color(r, g, b);
+                coords.push(x,y,z);
+                colors.push(r, g, b);
             }
         }
         
 
         const geo = new THREE.BufferGeometry()
-        geo.setAttribute('position', new THREE.Float32BufferAttribute(coords, 3))
+        geo.setAttribute("position", new THREE.Float32BufferAttribute(coords, 3));
         geo.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
-        const mat = new THREE.PointsMaterial({size: 0.3, vertexColors: true})
+        const mat = new THREE.PointsMaterial({size: 0.15, vertexColors: true})
         const points = new THREE.Points(geo, mat)
         scene.add(points)
 
 
-        renderer.render( scene, camera );
+        // renderer.render( scene, camera );
+        function animate() {
+            requestAnimationFrame( animate );           
+      
+            renderer.render( scene, camera );
+          };
+      
+          animate();
 
     },[])
     return (
